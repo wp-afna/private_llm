637fc4d616e54e90ee13eb61933ed135f91f8c2a
wp-afna <124837571+wp-afna@users.noreply.github.com>
2023-08-22 14:15:28 -0700

    Update ingest.py

--- ingest.py
+++ ingest.py
@@ -18,6 +18,7 @@ from langchain.document_loaders import (
     UnstructuredODTLoader,
     UnstructuredPowerPointLoader,
     UnstructuredWordDocumentLoader,
+    UnstructuredExcelLoader
 )
 
 from langchain.text_splitter import RecursiveCharacterTextSplitter
@@ -64,6 +65,7 @@ class MyElmLoader(UnstructuredEmailLoader):
 # Map file extensions to document loaders and their arguments
 LOADER_MAPPING = {
     ".csv": (CSVLoader, {}),
+    ".xlsx": (UnstructuredExcelLoader, {}),
     # ".docx": (Docx2txtLoader, {}),
     ".doc": (UnstructuredWordDocumentLoader, {}),
     ".docx": (UnstructuredWordDocumentLoader, {}),
